179788154799c28444ac15406f3660293f327140
duvan Notebook <duvan.vergara@usach.cl>
2025-04-29 22:19:19 -0400

    Mejorando para funcionamiento

--- kartingrm-backend/src/main/java/edu/mtisw/kartingrm/repositories/UserRepository.java
+++ kartingrm-backend/src/main/java/edu/mtisw/kartingrm/repositories/UserRepository.java
@@ -2,12 +2,10 @@ package edu.mtisw.kartingrm.repositories;
 
 import edu.mtisw.kartingrm.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    public UserEntity findByRut(String rut);
+    UserEntity findByRut(String rut);
 }
