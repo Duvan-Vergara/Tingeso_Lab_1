39014219b431746ff0796e9bd73ce70099ca55f3
duvan Notebook <duvan.vergara@usach.cl>
2025-04-30 00:24:58 -0400

    Mejorando para funcionamiento

--- kartingrm-backend/src/main/java/edu/mtisw/kartingrm/kartingrmBackendApplication.java
+++ kartingrm-backend/src/main/java/edu/mtisw/kartingrm/kartingrmBackendApplication.java
@@ -2,6 +2,9 @@ package edu.mtisw.kartingrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 
@@ -10,5 +13,19 @@ public class kartingrmBackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(kartingrmBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://4.206.177.191:8070") // Origen del frontend
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
